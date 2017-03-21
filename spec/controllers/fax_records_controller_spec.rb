bc8b14a46680d0fdb0ca935f5b5e603a4a45bf03
mwmaki <mwmaki2@gmail.com>
2017-03-21 15:39:31 -0500

    addin test for methods name and actions names also testing the column names for the schema

--- spec/controllers/fax_records_controller_spec.rb
+++ spec/controllers/fax_records_controller_spec.rb
@@ -2,4 +2,21 @@ require 'rails_helper'
 
 RSpec.describe FaxRecordsController, type: :controller do
 
+    list_of_all_actions_in_fax_records_controller = FaxRecordsController.action_methods.sort
+    describe "names for actions for the application should be the same in the test" do
+
+
+      it 'Search for action with name filtered_fax_records' do
+        expect(list_of_all_actions_in_fax_records_controller.grep(/index/)).to eq(['index'])
+      end
+
+      it 'Search for action with name to_csv' do
+        expect(list_of_all_actions_in_fax_records_controller.grep(/export/)).to eq(['export'])
+      end
+
+      it 'Search for action with name paginated_fax_record' do
+        expect(list_of_all_actions_in_fax_records_controller.grep(/send_fax/)).to eq(['send_fax'])
+      end
+
+    end
 end
--- spec/models/fax_record_spec.rb
+++ spec/models/fax_record_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe FaxRecord, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  list_of_all_methods_in_the_class = FaxRecord.methods.sort
+  describe "names for methods for the application should be the same in the test" do
+
+    it 'Search for Method with name filtered_fax_records' do
+      expect(list_of_all_methods_in_the_class.grep(:filtered_fax_records)).to eq([:filtered_fax_records])
+    end
+    it 'Search for Method with name to_csv' do
+      expect(list_of_all_methods_in_the_class.grep(:to_csv)).to eq([:to_csv])
+    end
+    it 'Search for Method with name paginated_fax_record' do
+      expect(list_of_all_methods_in_the_class.grep(:paginated_fax_record)).to eq([:paginated_fax_record])
+    end
+
+  end
+
 end
