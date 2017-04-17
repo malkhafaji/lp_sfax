df5cf11171e196c3e0ac4b022a388a5282770c1d
ssharba <samer.sharba@gmail.com>
2017-04-17 09:32:47 -0700

    fix cron schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,4 +1,4 @@
 every '*/5 9-17,0 * * *' do
-  system "rake check_fax_response"
-  system "rake sendback_final_response_to_client"
+  rake 'check_fax_response'
+  rake 'sendback_final_response_to_client'
 end
