f3697ce8c4a398200bd56cf031bb701aed73d906
Alkattan <aqattan@yahoo.com>
2017-05-12 18:13:19 -0500

    adding attachment model

--- app/controllers/api/v1/fax_records_controller.rb
+++ app/controllers/api/v1/fax_records_controller.rb
@@ -7,9 +7,9 @@ class Api::V1::FaxRecordsController < ApplicationController
   # Taking the fax_number,recipient_name and the attached file path and call the actual sending method to send the fax (made by the client)
   def send_fax
     begin
-      Rails.logger.debug "******* request for new fax **********************"
-      Rails.logger.debug params.inspect
-      Rails.logger.debug "**************************************************"
+      # Rails.logger.debug "******* request for new fax **********************"
+      # Rails.logger.debug params.inspect
+      # Rails.logger.debug "**************************************************"
       recipient_name = params['recipient_name']
       recipient_number = params['recipient_number']
       callback_url = params['FaxDispositionURL']
@@ -19,13 +19,15 @@ class Api::V1::FaxRecordsController < ApplicationController
       attachments_array.each_with_index do |file_info|
         attachments << file_path(file_info[0], file_info[1])
       end
-      fax_record =FaxRecord.new
+      fax_record = FaxRecord.new
       fax_record.client_receipt_date = Time.now
       fax_record.recipient_number = recipient_number
       fax_record.recipient_name = recipient_name
       fax_record.file_path = @original_file_name
       fax_record.callback_url = callback_url
       fax_record.save!
+      Rails.logger.debug "%%%%%%%%%%%%%#{fax_record.inspect}%%%%%%%%%%%%%%%%"
+      fax_record_attachment(fax_record, attachments_array)
       actual_sending(recipient_name, recipient_number, attachments, fax_record.id, fax_record.update_attributes(updated_by_initializer: false))
     rescue Exception => e
       render json: e.message.inspect
@@ -47,4 +49,9 @@ class Api::V1::FaxRecordsController < ApplicationController
     array_of_files_id_and_checksum.each_slice(2).to_a
   end
 
+  def fax_record_attachment(fax_record, attachments_array)
+    attachments_array.each do |file_info|
+      Attachment.create(fax_record_id: fax_record.id, file_id: file_info[0], checksum: file_info[1])
+    end
+  end
 end
