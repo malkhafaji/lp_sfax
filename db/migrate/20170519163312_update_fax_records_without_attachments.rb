133527c477d7108e1f9cea082e29bfb18bef02e4
Samer <samer.sharba@gmail.com>
2017-05-23 15:14:37 -0500

    add up/down on migration

--- db/migrate/20170519163312_update_fax_records_without_attachments.rb
+++ db/migrate/20170519163312_update_fax_records_without_attachments.rb
@@ -1,11 +1,18 @@
 class UpdateFaxRecordsWithoutAttachments < ActiveRecord::Migration[5.0]
 
-
+  def up
     FaxRecord.without_queue_id.each do |fax|
       unless fax.attachments.any?
         fax.update_attributes( result_message: "Transmission not completed", result_code: '7001', status: false )
       end
     end
+  end
 
-
+  def down
+    FaxRecord.without_queue_id.each do |fax|
+      unless fax.attachments.any?
+        fax.update_attributes( result_message: nil, result_code: nil, status: nil )
+      end
+    end
+  end
 end
