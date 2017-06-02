6d492b8a119e7df052e020ae2c72014211304647
Samer <samer.sharba@gmail.com>
2017-06-02 10:26:53 -0500

    refactoring send fax method

--- app/controllers/api/v1/fax_records_controller.rb
+++ app/controllers/api/v1/fax_records_controller.rb
@@ -3,57 +3,62 @@ include WebServices
 class Api::V1::FaxRecordsController < ApplicationController
   skip_before_action  :verify_authenticity_token
 
-
-  # Taking the fax_number,recipient_name and the attached file path and call the actual sending method to send the fax (made by the client)
   def send_fax
-    begin
-      Rails.logger.debug "==> request for new fax: #{params.inspect} <=="
-      recipient_name = params['recipient_name']
-      recipient_number = params['recipient_number']
-      callback_url = params['FaxDispositionURL']
-      attachments_array = params_to_array(params['Attachments'])
-      attachments = []
-      original_file_name = ''
-      attachments_array.each_with_index do |file_info|
-        file_info = WebServices::Web.file_path(file_info[0], file_info[1])
-        attachments << file_info[0]
-        original_file_name += file_info[1]
-      end
-      fax_record = FaxRecord.new
-      fax_record.client_receipt_date = Time.now
-      fax_record.recipient_number = recipient_number
-      fax_record.recipient_name = recipient_name
-      fax_record.file_path = original_file_name
-      fax_record.callback_url = callback_url
-      fax_record.updated_by_initializer = false
-      fax_record.save!
+    Rails.logger.debug "==> request for new fax: #{params.inspect} <=="
+    original_file_name, attachments_array = get_attachments(params_to_array(params['Attachments']))
+    fax_record = FaxRecord.new(recipient_name: params['recipient_name'],
+        recipient_number: params['recipient_number'], client_receipt_date: Time.now,
+        file_path: original_file_name, callback_url: params['FaxDispositionURL'],
+        updated_by_initializer: false)
+    if fax_record.save!
       fax_record_attachment(fax_record, attachments_array)
-      initial_response = FaxServices::Fax.actual_sending(recipient_name, recipient_number, attachments, fax_record.id)
-      render json: initial_response
-    rescue Exception => e
-      render json: e.message.inspect
+      render json: {status: 200}
+    else
       Rails.logger.debug "==> error send_fax: #{e.message.inspect} <=="
+      render json: fax_record.errors
     end
   end
 
   private
-  def params_to_array(string)
-    array_of_files_id_and_checksum = []
-    j,u = 0,0
-    parsed_string= string.split(/[\s,+=]/)
-    parsed_string.each do |i|
-      j = j+1
-        if j % 2 == 0
-         array_of_files_id_and_checksum = ( (u % 2 == 0) ? array_of_files_id_and_checksum.push(i.to_i) : array_of_files_id_and_checksum.push(i.to_s) )
-          u=u+1
-        end
+    def params_to_array(string)
+      array_of_files_id_and_checksum = []
+      j,u = 0,0
+      parsed_string= string.split(/[\s,+=]/)
+      parsed_string.each do |i|
+        j = j+1
+          if j % 2 == 0
+           array_of_files_id_and_checksum = ( (u % 2 == 0) ? array_of_files_id_and_checksum.push(i.to_i) : array_of_files_id_and_checksum.push(i.to_s) )
+            u=u+1
+          end
+      end
+      array_of_files_id_and_checksum.each_slice(2).to_a
     end
-    array_of_files_id_and_checksum.each_slice(2).to_a
-  end
 
-  def fax_record_attachment(fax_record, attachments_array)
-    attachments_array.each do |file_info|
-      Attachment.create(fax_record_id: fax_record.id, file_id: file_info[0], checksum: file_info[1])
+    def fax_record_attachment(fax_record, attachments_array)
+      attachments_array.each do |file_info|
+        Attachment.create(fax_record_id: fax_record.id, file_id: file_info[0], checksum: file_info[1])
+      end
     end
-  end
+
+    def create_initial_response(fax_record)
+      initial_response = {
+        fax_id: fax_record.id,
+        recipient_name: fax_record.recipient_name,
+        recipient_number: fax_record.recipient_number,
+        message: fax_record.message,
+        status: fax_record.status,
+        result_message: fax_record.result_message}
+      return initial_response
+    end
+
+    def get_attachments(array)
+      original_file_name = ''
+      attachments = []
+      array.each_with_index do |file_info|
+        file_info = WebServices::Web.file_path(file_info[0], file_info[1])
+        attachments << file_info[0]
+        original_file_name += file_info[1]
+      end
+      return original_file_name, attachments
+    end
 end
