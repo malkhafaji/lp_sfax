8e21d326c6645a2f92c38df194f515769ce9d5e1
Samer <samer.sharba@gmail.com>
2017-07-06 14:11:36 -0500

    PRS-44 add new column to attachment

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,4 +1,4 @@
 class Attachment < ApplicationRecord
   belongs_to :fax_record
-  validates_presence_of :fax_record_id, :file_id, :checksum
+  validates_presence_of :fax_record_id, :file_unique_key
 end
