4864417da44f744b69c95ddf91739aea9a2e9ff6
louy755 <lumberjack1695@yahoo.com>
2017-08-03 15:14:35 -0500

    PRS-49 add ActionMailer to email system exceptions to admin

---
+++ app/mailers/notification_mailer.rb
@@ -0,0 +1,6 @@
+class NotificationMailer < ApplicationMailer
+	def sys_error(e)
+		 @message = e
+	  mail(to: "louy@gilgabytes.com", subject: "Exception Accrue Need your Attention", body: @message )
+	end
+end
