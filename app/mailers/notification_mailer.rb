dedb789c7eedc6bbc2ffb2511f63a2a81e94533f
louy755 <lumberjack1695@yahoo.com>
2017-08-04 14:35:19 -0500

    PRS-45 modify the app name

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,6 +1,6 @@
 class NotificationMailer < ApplicationMailer
 	def sys_error(e)
 		 @message = e
-	  mail(to:  ENV['GMAIL_ADMIN'], subject:["#{Rails.env}""  ""Error on ""#{File.basename(Rails.root.to_s)}"], body: @message )
+	  mail(to:  ENV['GMAIL_ADMIN'], subject:["#{Rails.env}""  ""Error on ""#{Rails.application.class.parent_name}"], body: @message )
 	end
 end
