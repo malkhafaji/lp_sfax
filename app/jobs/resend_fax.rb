bde19f01d71decfe88ea6ccf43fefe2fe0a88116
Samer <samer.sharba@gmail.com>
2017-08-04 13:50:46 -0500

    code cleaning

--- app/jobs/resend_fax.rb
+++ app/jobs/resend_fax.rb
@@ -5,10 +5,10 @@ class ResendFax
 
   sidekiq_options queue: 'resend_fax'
 
-  def perform(fax_record_id)
-    Rails.logger.debug("==> Resending the fax with ID = #{fax_record_id} <==")
+  def perform(id)
+    Rails.logger.debug("==> Resending the fax with ID = #{id} <==")
     FileUtils.rm_rf Dir.glob("#{Rails.root}/tmp/fax_files/*")
-    fax_record = FaxRecord.find_by_id(fax_record_id)
+    fax_record = FaxRecord.find(id)
     attachments_array = fax_record.attachments.pluck('file_key')
     attachments=  WebServices::Web.file_path(attachments_array)
     FaxServices::Fax.actual_sending( fax_record.recipient_name, fax_record.recipient_number, attachments, fax_record.id)
