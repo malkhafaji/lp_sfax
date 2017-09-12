4691b6ab045f24c7de37ee075451e631a65ea156
Samer <samer.sharba@gmail.com>
2017-09-12 12:00:57 -0500

    removed large migrations to tasks

--- lib/tasks/bulk_data_processing.rb
+++ lib/tasks/bulk_data_processing.rb
@@ -1,5 +1,6 @@
-class UpdateFileKeyForOldRecords < ActiveRecord::Migration[5.0]
-  def change
+namespace :bulk_data_processing do
+  desc 'Generate unique key for documents without key'
+  task add_unique_key: :environment do
     Attachment.where.not(checksum: nil).find_each do |a|
       puts "updating file #{a.file_id}, #{a.checksum}"
       url="#{ENV['file_service_path']}/api/v1/documents/get_file_key?file_id=#{a.file_id}&checksum=#{a.checksum}"
@@ -7,5 +8,4 @@ class UpdateFileKeyForOldRecords < ActiveRecord::Migration[5.0]
       res_json = JSON.parse(response.body)
       a.update_attributes(file_key: res_json["unique_key"])
     end
-  end
 end
