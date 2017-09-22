f94a82d4d4eb75285586d8f038ad48c3aadc260f
Samer <samer.sharba@gmail.com>
2017-09-22 10:30:15 -0500

    fix setup method

--- db/migrate/20170829175233_create_callback_servers.rb
+++ db/migrate/20170829175233_create_callback_servers.rb
@@ -3,8 +3,8 @@ class CreateCallbackServers < ActiveRecord::Migration[5.0]
     create_table :callback_servers do |t|
       t.string :name
       t.string :url, index: true, null: false
-      t.string :update_url, index: true, null: false
-      t.integer :insert_port, index: true, null: false
+      t.string :update_url, index: true
+      t.integer :insert_port, index: true
       t.timestamps
     end
     add_column :fax_records, :callback_server_id, :integer
