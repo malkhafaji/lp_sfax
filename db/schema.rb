820fd8eeefc18090ea6318b6095b3d7446ad2751
Samer <samer.sharba@gmail.com>
2017-09-27 14:16:05 -0500

    add migration to update old record with server id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170913014005) do
+ActiveRecord::Schema.define(version: 20170927190713) do
 
   create_table "attachments", force: :cascade do |t|
     t.integer  "fax_record_id"
