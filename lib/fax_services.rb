a3900704c1f323ce64e4e1ee366c97f216c2b832
Samer <samer.sharba@gmail.com>
2017-10-03 10:56:44 -0500

    fix bug in resend condition

--- lib/fax_services.rb
+++ lib/fax_services.rb
@@ -31,7 +31,7 @@ module FaxServices
         attachments, file_dir=  WebServices::Web.file_path(attachments_keys, fax_id)
         if (attachments.empty?) || (attachments.size != attachments_keys.size)
           fax_record.update_attributes(message: 'Fax request is complete', result_message: "No files found to download for fax with ID: #{fax_id}", error_code: 1515102, result_code: 7002, status: false, is_success: false)
-          HelperMethods::Logger.app_logger('error', "==> No files found to download for fax with ID: #{fax_id}")
+          HelperMethods::Logger.app_logger('error', "send_now: No files found to download for fax with ID: #{fax_id}")
           InsertFaxJob.perform_async(fax_record.id)
           FileUtils.rm_rf Dir.glob(file_dir)
           return
@@ -64,7 +64,7 @@ module FaxServices
           max_fax_response_check_tries: 0,
         send_confirm_date: response['date'])
         if fax_record.send_fax_queue_id.nil?
-          HelperMethods::Logger.app_logger('error', "==> error send_fax_queue_id is nil: #{response_result} <==")
+          HelperMethods::Logger.app_logger('error', "send_now: error send_fax_queue_id is nil: #{response_result}")
           fax_record.update_attributes(message: 'Fax request is complete', result_message: 'Transmission not completed', error_code: 1515101, result_code: 7001, status: false, is_success: false)
           InsertFaxJob.perform_async(fax_record.id)
         end
@@ -91,7 +91,7 @@ module FaxServices
             fax.update_attributes( updated_by_initializer: true )
             FaxServices::Fax.send_now(fax.id)
           rescue
-            HelperMethods::Logger.app_logger('error', "==> Error sending_faxes_without_queue_id: #{fax.id} <==")
+            HelperMethods::Logger.app_logger('error', "sending_faxes_without_queue_id: Error sending_faxes_without_queue_id: #{fax.id}")
           end
         end
       end
@@ -111,9 +111,9 @@ module FaxServices
         client_receipt_date: fax_record.client_receipt_date}
         InsertFaxJob.perform_async(fax_record.id)
         if fax_record.updated_by_initializer == true
-          HelperMethods::Logger.app_logger('info', "==> sendback_initial_response_to_client/updated_by_initializer: #{client_initial_response} <==")
+          HelperMethods::Logger.app_logger('info', "sendback_initial_response_to_client/updated_by_initializer: #{client_initial_response}")
         else
-          HelperMethods::Logger.app_logger('info',  "==> sendback_initial_response_to_client: #{client_initial_response} <==")
+          HelperMethods::Logger.app_logger('info',  "sendback_initial_response_to_client: #{client_initial_response}")
           client_initial_response
         end
       end
@@ -129,7 +129,7 @@ module FaxServices
           if response["RecipientFaxStatusItems"].present?
             fax_record = FaxRecord.find_by_send_fax_queue_id(fax_requests_queue_id)
             parse_response = response["RecipientFaxStatusItems"][0]
-            unless fax_record.resend <= ENV['MAX_RESEND'].to_i && parse_response['ResultCode'] == 6000
+            unless parse_response['ResultCode'] == 6000 && fax_record.resend <= ENV['MAX_RESEND'].to_i
               HelperMethods::Logger.app_logger('info', "fax_response: #{parse_response}")
               if parse_response['ResultCode'] == 0
                 fax_duration = calculate_duration(fax_record.client_receipt_date, (Time.parse(parse_response['FaxDateUtc'])))
@@ -161,10 +161,10 @@ module FaxServices
                 result_message:      result_message,
                 fax_duration:        fax_duration
               )
-            else
+            elsif !fax_record.in_any_queue?
               HelperMethods::Logger.app_logger('info', "fax_response: Resend fax with ID = #{fax_record.id}")
               fax_record.update_attributes(resend: (fax_record.resend+1))
-              ResendFaxJob.perform_in((ENV['DELAY_RESEND'].to_i).minutes, fax_record.id) unless fax_record.in_any_queue?
+              ResendFaxJob.perform_in((ENV['DELAY_RESEND'].to_i).minutes, fax_record.id)
             end
           else
             HelperMethods::Logger.app_logger('info', 'fax_response: no response found')
@@ -203,18 +203,18 @@ module FaxServices
         records_groups = FaxRecord.not_send_to_client
         records_groups.each do |server_id, records|
           callback_server = CallbackServer.find(server_id)
-          HelperMethods::Logger.app_logger('info', "==> total #{records.size} records for #{callback_server.name} <==")
+          HelperMethods::Logger.app_logger('info', "total #{records.size} records for #{callback_server.name}")
           array_of_records =  prepare_client_date(records)
           if array_of_records.blank?
-            HelperMethods::Logger.app_logger('info', '==> sendback_final_response_to_client: No responses for faxes found <==')
+            HelperMethods::Logger.app_logger('info', 'sendback_final_response_to_client: No responses for faxes found')
           else
             array_in_batches = array_of_records.each_slice(ENV['max_records_send_to_client'].to_i).to_a
             array_in_batches.each do |batch_of_records|
               begin
-                HelperMethods::Logger.app_logger('info', "==> #{Time.now} posting #{batch_of_records.size} records to #{callback_server.name} <==")
+                HelperMethods::Logger.app_logger('info', "#{Time.now} posting #{batch_of_records.size} records to #{callback_server.name}")
                 url = URI(callback_server.update_url+'/eFaxService/OutboundDispositionService.svc/Receive')
                 response = HTTParty.post(url, body: batch_of_records.to_json, headers: { 'Content-Type' => 'application/json' } )
-                HelperMethods::Logger.app_logger('info', "==> #{Time.now} end posting <==")
+                HelperMethods::Logger.app_logger('info', "#{Time.now} end posting")
                 if response.present? && response.code == 200
                   result = JSON.parse(response)
                   success_ids = []
@@ -224,12 +224,12 @@ module FaxServices
                       FaxRecord.find(r['Fax_Id']).update_attributes(sendback_final_response_to_client: 1)
                     end
                   end
-                  HelperMethods::Logger.app_logger('info', "==> successfully updated: #{success_ids} <==")
+                  HelperMethods::Logger.app_logger('info', "successfully updated: #{success_ids}")
                 else
-                  HelperMethods::Logger.app_logger('error', "==> response error: #{response} <==")
+                  HelperMethods::Logger.app_logger('error', "final_response_to_client: response error(#{response})")
                 end
               rescue Exception => e
-                HelperMethods::Logger.app_logger('error', "==> Error while posting final response: #{e.message}")
+                HelperMethods::Logger.app_logger('error', "final_response_to_client: Error while posting final response(#{e.message})")
               end
             end
           end
@@ -287,13 +287,13 @@ module FaxServices
       def service_alive?
         if fax_vendor_up?
           unless VendorStatus.service_up?
-            HelperMethods::Logger.app_logger('error', "FaxService is up #{Time.now}")
+            HelperMethods::Logger.app_logger('info', "FaxService is up #{Time.now}")
             VendorStatus.create!(service: 'up')
           end
           return true
         else
           unless VendorStatus.service_down?
-            HelperMethods::Logger.app_logger('error', "FaxService is down #{Time.now}")
+            HelperMethods::Logger.app_logger('info', "FaxService is down #{Time.now}")
             VendorStatus.create!(service: 'down')
           end
           return false
