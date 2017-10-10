147b8514301be9ab37462c1137ebf10c57bafa69
Samer <samer.sharba@gmail.com>
2017-10-09 19:38:18 -0500

    fix logic issue and DRY

--- app/controllers/fax_records_controller.rb
+++ app/controllers/fax_records_controller.rb
@@ -53,53 +53,22 @@ class FaxRecordsController < ApplicationController
     end
   end
 
-  def not_send_fax_queue_id
-    @fax_records = FaxRecord.all
-  end
   def report
-    @desierd_month = params[:desierd_month] ||= Date.today.strftime("%m")
+    @desierd_month = params[:desierd_month] ||= Date.today.strftime('%m')
+    @fax_records = FaxRecord.by_month(@desierd_month).where.not(send_fax_queue_id: nil)
     @month_name = Date::MONTHNAMES[@desierd_month.to_i]
     @types_hash = Hash.new(0)
-    queue_ids_not_sent = FaxRecord.by_month(@desierd_month).where.not(send_fax_queue_id: nil, result_message: "Success")
-    queue_ids_not_sent.each do |fax_record|
+    failed_faxes = @fax_records.where.not(result_message: 'Success')
+    failed_faxes.each do |fax_record|
       message_type = fax_record.result_message
       @types_hash[message_type] += 1 unless  message_type == nil
     end
-    change_display_status
-    calculate_fax_percentage
-
-    respond_to do |format|
-      format.html
-    end
-  end
-
-  private
-
-  def calculate_fax_percentage
-    total_reports  = @chart_display['Success'] + @chart_display['Fail']
-    @success_percentage = (@chart_display['Success']/total_reports.to_f * 100).to_i
-    @fail_percentage = 100 - @success_percentage
-  end
-
-  def change_display_status
+    total_sccess = @fax_records.where(is_success: 't')
+    @success = ((total_sccess.size.to_f / @fax_records.size) * 100).to_i
     @chart_display = {}
-    @fax_records = FaxRecord.by_month(@desierd_month)
     records = @fax_records.group(:is_success).count
     records.each do |key, value|
-      if key == 't'
-        @chart_display['Success'] = records[key]
-      else
-        @chart_display['Fail'] = records[key]
-      end
+      key == 't' ? @chart_display['Success'] = records[key] :  @chart_display['Fail'] = records[key]
     end
   end
-  # # Use callbacks to share common setup or constraints between actions.
-  # def set_fax_record
-  #   @fax_record = FaxRecord.find(params[:id])
-  # end
-  #
-  # # Never trust parameters from the scary internet, only allow the white list through.
-  # def fax_record_params
-  #   params.require(:fax_record).permit(:recipient_name, :recipient_number, :file_path, :client_receipt_date, :status, :SendFaxQueueId, :message, :max_fax_response_check_tries, :send_confirm_date, :vendor_confirm_date, :send_fax_queue_id, :is_success, :result_code, :error_code, :result_message, :recipient_fax, :tracking_code, :fax_date_utc, :fax_id, :pages, :attempts, :sender_fax, :barcode_items, :fax_success, :out_bound_fax_id, :fax_pages, :fax_date_iso, :watermark_id, :message)
-  # end
 end
