69701405033dcea048e4bba0cbed919580cb65e3
Samer <samer.sharba@gmail.com>
2017-10-12 14:56:14 -0500

    fix issue with sessions

--- app/assets/javascripts/session.js
+++ app/assets/javascripts/session.js
@@ -1,70 +1,61 @@
 $(document).ready(function() {
   // Delay before the modal comes up (milliseconds)
-     var delay = 15 * 60000;
+  var delay = 15 * 60000;
   // Delay after displaying modal before moving to different location
-     var reactionDelay = 15 * 60000;
-     var logoutHref = $("ul.dropdown>li#nav-li>#nav-a").attr("href");
+  var reactionDelay = 15 * 60000;
 
-     $("#nav-ul #nav-li").on('click', function(){
-       if($("#nav-ul.dropdown").is(":visible")){
-         $("#nav-ul.dropdown").hide();
-       }
-       else{
-         $("#nav-ul.dropdown").show();
-       }
+  var logoutHref = $("ul.dropdown-menu#logout>li#nav-li>#nav-a").attr("href");
+  $("#nav-ul #nav-li").on('click', function(){
+    if($("#nav-ul.dropdown-menu").is(":visible")){
+      $("#nav-ul.dropdown-menu").hide();
+    }else{
+      $("#nav-ul.dropdown-menu").show();
+    }
+  });
+  if(logoutHref == null)
+    return;
+  var $modal = $('.modal-mask');
+  var $modalBtn = $modal.find(".modal-btn");
+  var lastActive = null;
+  var modalShown = null;
+  var getNow = performance && performance.now ? function(){ return performance.now() } : function(){ return Date.now();}
+  var showAFKModal = function showAFKModal() {
+    modalShown = getNow();
+    $(document.body).append($modal);
+    $modal.show();
+    $('.cont').toggleClass('modal-blur');
+  };
 
-     });
+  var checkTimeout = function checkTimeout() {
+    var now = getNow();
+    if(!modalShown){
+      if(now - lastActive  > delay){
+        showAFKModal();
+      }
+    }else{
+      if (now - modalShown  > reactionDelay) {
+        window.location = logoutHref;
+      }
+    }
+  };
 
-     if(logoutHref == null)
-        return;
-     var $modal = $('.modal-mask');
+  $modalBtn.on("click", function(e) {
+    e.preventDefault();
+    $modal.hide();
+    $('.cont').toggleClass('modal-blur');
+    $modal.detach();
+    modalShown = false;
+  });
 
-     var $modalBtn = $modal.find(".modal-btn");
-     var lastActive = null;
-     var modalShown = null;
+  lastActive = getNow();
 
-     var getNow = performance && performance.now ? function(){ return performance.now() } : function(){ return Date.now();}
-     var showAFKModal = function showAFKModal() {
+  $(document).on("click keydown keyup focus mousemove",function(){
+    lastActive = getNow();
+  });
 
-        modalShown = getNow();
-        $(document.body).append($modal);
-        $modal.show();
-        $('.cont').toggleClass('modal-blur');
-
-     };
-
-       var checkTimeout = function checkTimeout() {
-         console.log("called!");
-       var now = getNow();
-       if(!modalShown){
-        if(now - lastActive  > delay){
-         showAFKModal();
-         }
-        }
-        else{
-          if (now - modalShown  > reactionDelay) {
-           window.location = logoutHref;
-         }
-       }
-       };
-
-      $modalBtn.on("click", function(e) {
-      e.preventDefault();
-       $modal.hide();
-       $('.cont').toggleClass('modal-blur');
-       $modal.detach();
-       modalShown = false;
-      });
-       lastActive = getNow();
-
-      $(document).on("click keydown keyup focus mousemove",function(){
-      lastActive = getNow();
-      });
-
-      $(window).on("click keydown keyup focus mousemove",function(){
-      lastActive = getNow();
-      })
-
-       setInterval(checkTimeout,1000);
+  $(window).on("click keydown keyup focus mousemove",function(){
+    lastActive = getNow();
+  })
+  setInterval(checkTimeout,1000);
 
  });
