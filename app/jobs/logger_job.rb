0409f813e4130542f2c35995ec19d4f09ddced65
mwmaki <mwmaki2@gmail.com>
2017-11-21 14:40:37 -0600

    adding logger job and helper method

--- app/jobs/logger_job.rb
+++ app/jobs/logger_job.rb
@@ -4,30 +4,22 @@ class LoggerJob
   sidekiq_options queue: 'logger'
 
   def perform(fax_id)
-  	HelperMethods::Logger.app_logger('info', "Logging Fax with fax ID: #{fax_id}")
-    fax_record = FaxRecord.find(fax_id)
-    url = URI(fax_record.callback_server.url+'/DataAccessService/sFaxService.svc/InsertFaxes')
-    url.port = fax_record.callback_server.insert_port
+    data = HelperMethods::Logger.logger_service_message(fax_id)
+    url = URI(ENV['LOGGER_SERVICE_HOST'])
+    url.port = ENV['LOGGER_SERVICE_PORT']
     http = Net::HTTP.new(url.host, url.port)
-    data = {
-      recipient_name: fax_record.recipient_name
-      recipient_number: fax_record.recipient_number
-      FaxDispositionURL: fax_record.params['FaxDispositionURL']
-      Attachments: fax_record.params['Attachments']
-      e_sk: fax_record.callback_param['e_sk']
-      let_sk: fax_record.callback_param['let_sk']
-      type_cd_sk: fax_record.callback_param['type_cd_sk']
-      priority_cd_sk: fax_record.callback_param['priority_cd_sk']
-    }
+    http.use_ssl = true                      # CHECK MAYBE NEED TO BE TURNED OFF OR REMOVE IT !
     request = Net::HTTP::Post.new(url, {'Content-Type' => 'application/json'})
     request.body = data.to_json
     response = http.request(request)
-    if response.present? && response.code == '200'
-      HelperMethods::Logger.app_logger('info', "Logger: #{data.to_json}")
-      HelperMethods::Logger.app_logger('info', "Logger response: #{response.body}")
+
+    if response.code == '200'
+      HelperMethods::Logger.app_logger('info', "logger service message: #{data.to_json}")
+      HelperMethods::Logger.app_logger('info', "logger service response: #{response.body}")
     else
-      HelperMethods::Logger.app_logger('error', response.inspect)
+      HelperMethods::Logger.app_logger('error', "LoggerJob: #{response.inspect}")
       raise response.inspect
     end
+
   end
 end
--- lib/helper_methods.rb
+++ lib/helper_methods.rb
@@ -1,8 +1,8 @@
 module HelperMethods
   class Logger
     class << self
-  def app_logger(type, message)
-      case type
+      def app_logger(type, message)
+        case type
         when type = 'error'
           Rails.logger.error message
           NotificationMailer.app_error(message).deliver_later if Rails.env.production?
@@ -13,6 +13,31 @@ module HelperMethods
         when type = 'warn'
           Rails.logger.warn message
         end
+      end
+
+      def logger_service_message(id)
+        data = {
+          client_id:'0000',
+          source_app: ['ISO', 'IQ', 'Fax'],
+          is_sensitive: ['true/fasle'],
+          action: ['create', 'update','delete', 'service'],
+          actor: ['jsmith'],
+          actor_type: [0,1,2,3],
+          extended_attributes: ['true/false'],
+          event: ['the message that we need to log'],
+          event_type: ['info', 'error', 'debug', 'warning'],
+          process_id:['optional',1212121],
+          thread_id:['optional',2121212],
+          session_id: ['a3b5d555ef'],
+          params: ['any number of attributes key => values'],
+          loggerable: {
+            type: 'Fax',
+            fax_id:01,
+            client_id:'01',
+            recipient_number:00000000000,
+            recipient_name:'test',
+          no_of_attachments:0 }
+        }
       end
     end
   end
