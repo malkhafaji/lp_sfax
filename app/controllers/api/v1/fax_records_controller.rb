f3314d19c75c5287e8fd11b4bcbe3008eef032d5
Alkattan <aqattan@yahoo.com>
2017-12-01 17:20:53 -0600

    rake task for client_id and add client_id to send fax method

--- app/controllers/api/v1/fax_records_controller.rb
+++ app/controllers/api/v1/fax_records_controller.rb
@@ -1,7 +1,7 @@
 require 'open-uri'
 class Api::V1::FaxRecordsController < ApplicationController
   skip_before_action  :verify_authenticity_token, :authenticate_user!
-
+  
   def show
     fax_record = FaxRecord.find(params[:id])
 
@@ -14,7 +14,7 @@ class Api::V1::FaxRecordsController < ApplicationController
 
   def send_fax
     begin
-      unless params['recipient_name'].present? && params['recipient_number'].present? && params['FaxDispositionURL'].present? && params['Attachments'].present? && params['e_sk'].present? && params['let_sk'].present? && params['type_cd_sk'].present? && params['priority_cd_sk'].present?
+      unless params['recipient_name'].present? && params['recipient_number'].present? && params['FaxDispositionURL'].present? && params['Attachments'].present? && params['e_sk'].present? && params['let_sk'].present? && params['type_cd_sk'].present? && params['priority_cd_sk'].present? && params['client_id'].present? 
         raise ActionController::ParameterMissing.new('required params')
       end
       callback_server = CallbackServer.find_by_url(params['FaxDispositionURL'])
@@ -23,7 +23,7 @@ class Api::V1::FaxRecordsController < ApplicationController
       end
       HelperMethods::Logger.app_logger('info', "==> request for new fax: #{params.inspect} <==")
       attachments_array = params_to_array(params['Attachments'])
-      fax_record = FaxRecord.new(callback_server_id: callback_server.id, client_receipt_date: Time.now, recipient_number: params['recipient_number'], recipient_name: params['recipient_name'], updated_by_initializer: false)
+      fax_record = FaxRecord.new(callback_server_id: callback_server.id, client_receipt_date: Time.now, recipient_number: params['recipient_number'], recipient_name: params['recipient_name'], updated_by_initializer: false, client_id: params['client_id'])
       respond_to do |format|
         if fax_record.save
           CallbackParam.create(let_sk: params['let_sk'], e_sk: params['e_sk'], type_cd_sk: params['type_cd_sk'], priority_cd_sk: params['priority_cd_sk'], fax_record_id: fax_record.id)
