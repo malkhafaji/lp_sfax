bb21dcca933097e8723f05e39cee99d95924ecb2
Alkattan <aqattan@yahoo.com>
2017-12-04 12:51:12 -0600

    Changing logout color

--- db/migrate/20171201171717_rename_refresh_token_to_encrypted_password.rb
+++ db/migrate/20171201171717_rename_refresh_token_to_encrypted_password.rb
@@ -1,5 +1,6 @@
 class RenameRefreshTokenToEncryptedPassword < ActiveRecord::Migration[5.0]
   def change
     rename_column :users, :refresh_token, :encrypted_password
+    add_column :users, :timeout, :integer
   end
 end
