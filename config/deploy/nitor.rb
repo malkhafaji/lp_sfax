a247f1dda2702edf8585498d59730817b8d8a978
Samer <samer.sharba@gmail.com>
2017-12-09 09:12:32 -0600

    change deployment address

--- config/deploy/nitor.rb
+++ config/deploy/nitor.rb
@@ -2,5 +2,5 @@
 # ======================
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
-server '54.209.151.121', user: 'deploy', roles: %w{app db web}, my_property: :my_value
+server '52.43.20.177', user: 'deploy', roles: %w{app db web}, my_property: :my_value
 set :rails_env, 'development'
